ddcba07675b2fa3267d846a3e692600950f12123
AST <masterminds0809@gmail.com>
2023-05-03 22:01:39 +0530

    ok

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express,{json} from 'express';
 import cors from 'cors';
 import {db,connectToDB} from './database.js';
+import { name } from 'ejs';
 const app=express();
 app.use(express.json());
 app.use(cors());
@@ -11,8 +12,9 @@ app.post('/register/:name/:gmail/:password/:cpassword',async(req,res)=>{
     const details=await db.collection('userdata').insertOne({name:req.params.name,gmail:req.params.gmail,password:req.params.password,cpassword:req.params.cpassword})
     res.json(details);
 })
-app.post('/login/:gamil/:password',async(req,res)=>{
-    const details=await db.connectToDB('userdata').find().toArray();
+app.get('/login',async(req,res)=>{
+    const details=await db.collection('userdata').find().toArray();
+    // res.send(details);
     res.json(details);
 })
 connectToDB(()=>{
