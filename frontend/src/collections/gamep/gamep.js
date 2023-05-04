2aef477e90eba45fbaafe0a031d5c6fed3607619
AST <masterminds0809@gmail.com>
2023-05-05 00:21:46 +0530

    ok

--- frontend/src/collections/gamep/gamep.js
+++ frontend/src/collections/gamep/gamep.js
@@ -1,22 +1,40 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 const Gamep=()=>
 {
+    const nav=useNavigate();
+    const Coding=()=>
+    {
+        nav("/codinggame")
+    }
+    const Current=()=>
+    {
+        nav("/currentgame")
+    }
+    const Sports=()=>
+    {
+        nav("/sportsgame")
+    }
+    const Entertine=()=>
+    {
+        nav("/entertinegame")
+    }
     return(
         <>
        <div className="lbtn">
         <div className="btn1">
-            <button className="gbtn"><b>Enter</b></button>
+            <button className="gbtn" onClick={Coding}><b>Enter</b></button>
         </div>
         <div className="btn2">
-        <button className="gbtn"><b>Enter</b></button>
+        <button className="gbtn" onClick={Sports}><b>Enter</b></button>
         </div>
         </div>
         <div className="lbtnr">
         <div className="btn3">
-        <button className="gbtn"><b>Enter</b></button>
+        <button className="gbtn" onClick={Current}><b>Enter</b></button>
         </div>
         <div className="btn4">
-        <button className="gbtn"><b>Enter</b></button>
+        <button className="gbtn" onClick={Entertine}><b>Enter</b></button>
         </div>
         </div>
         </>
