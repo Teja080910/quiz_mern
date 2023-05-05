4cc3a378f78ec8941b7616b61bb9922fec86a5f6
AST <masterminds0809@gmail.com>
2023-05-05 14:30:10 +0530

    ok

--- frontend/src/collections/error/error.js
+++ frontend/src/collections/error/error.js
@@ -2,9 +2,9 @@ import React from "react";
 const Errorpage=()=>{
     return(
         <>
-        <div id="not found">
-            <div className="not found">
-                <div className="not found-404">
+        <div>
+            <div>
+                <div>
                     <h1>404</h1>
                 </div>
                 <h2>we are sorry,page not found.!</h2>
