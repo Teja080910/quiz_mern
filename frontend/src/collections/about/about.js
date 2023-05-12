3ade7b2cb87edd9f4b22811bbf2bc342b0927fe6
sailakshmi004 <sailakshmiborra4102@gmail.com>
2023-05-12 21:37:40 +0530

    hello

--- frontend/src/collections/about/about.js
+++ frontend/src/collections/about/about.js
@@ -8,8 +8,8 @@ const About=()=>{
         <div className="about">
         <h1>ABOUT</h1>
         <p>This is the Quiz app application , 
-            In this the fisrt page is Start when you click start button you will be directed to the next page with Admin & User.If you are a existed user you can do direct login otherwise you should create new account and do Register.And then do login.
-            You can also login by clicking forgot password.</p>
+            In this the fisrt page is Start when you click start button you will be directed to the next page with Admin & User. If you are a existed user you can do direct login otherwise you should register to new account. And then do login.
+            You can also login by clicking forgot password. </p>
             Once the login is completed you will be the quiz page with four categories namely
             <ul>
              <li>CODING</li> 
@@ -18,8 +18,8 @@ const About=()=>{
              <li>ENTERTAINMENT</li>
              </ul>
             When you click enter the quiz will starts,<br></br>
-            For each question your marks will be increased by two marks.<br></br>
-            And for wrong attempt there is negative marking i.e your score will decreased by one mark.<br></br>
+            For each correct attempt your marks will be increased by two.<br></br>
+            And for wrong attempt there is negative marking i.e your score will decreased by one.<br></br>
             After completing the quiz , You can check your score in the leaderboard.
         </div>
         </>
